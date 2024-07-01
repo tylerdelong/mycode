d6453a1aadeff71098686ac4505814bab0df7748
Tyler <Tyler>
2024-07-01 20:54:17 +0000

    mod to input challenge

---
+++ lab_input/mod1iputchallenge.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def main():
+    
+    #name input var
+    name = input("What is your name?:")
+    #day input var
+    day = input("What day is it?:")
+
+    #print statment
+    print("Hello,", name + "!", "Happy", day + "!")
+
+main()
